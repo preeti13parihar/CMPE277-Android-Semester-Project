25c6475de42956441b3ad4a1d5f1659dfe606462
preeti1998parihar <preeti1998parihar@gmail.com>
2021-05-18 10:12:49 -0700

    final commit

---
+++ app/src/main/java/com/cmpe277/newsflash/welcome/fragment/ExampleFragment.java
@@ -0,0 +1,55 @@
+package com.cmpe277.newsflash.welcome.fragment;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import androidx.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.stephentuso.welcome.WelcomePage;
+import com.stephentuso.welcome.WelcomeUtils;
+import com.cmpe277.newsflash.R;
+
+public class ExampleFragment extends Fragment implements WelcomePage.OnChangeListener {
+
+    private ViewGroup rootLayout;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_example, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        rootLayout = (ViewGroup) view.findViewById(R.id.layout);
+
+        view.findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "Button clicked in custom fragment", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public void onWelcomeScreenPageScrolled(int pageIndex, float offset, int offsetPixels) {
+        if (rootLayout != null)
+            WelcomeUtils.applyParallaxEffect(rootLayout, true, offsetPixels, 0.3f, 0.2f);
+    }
+
+    @Override
+    public void onWelcomeScreenPageSelected(int pageIndex, int selectedPageIndex) {
+        //Not used
+    }
+
+    @Override
+    public void onWelcomeScreenPageScrollStateChanged(int pageIndex, int state) {
+        //Not used
+    }
+}
